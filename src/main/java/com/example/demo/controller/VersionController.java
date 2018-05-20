bfb800f6390b9970df51fbdb38a8c38c1db20dde
linwenjun <linwenjun@163.com>
2018-05-20 10:31:46 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/VersionController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/version")
+public class VersionController {
+
+    private int major = 1;
+    private int minor = 0;
+    private int patch = 0;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getVersion() {
+        return major + "." + minor + "." + patch;
+    }
+
+    @RequestMapping(path = "/major", method = RequestMethod.POST)
+    public String updateMajor() {
+        major++;
+        return getVersion();
+    }
+}
