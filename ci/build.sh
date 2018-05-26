3b860936dfe3665259bd003f1f2d403c364ad66c
Seaborn Lee <seabornlee.cn@gmail.com>
2018-05-26 21:37:39 +0800

    Run SonarQube

--- ci/build.sh
+++ ci/build.sh
@@ -5,6 +5,8 @@ cd git-resource
 # update version
 sed "s/DOCKER_VERSION/$(cat ../version/number)/g" ci/deployment.yml > ../build/deployment.yml
 
+gradle sonarqube
+
 # package
 gradle build
 cp build/libs/*.jar ../build
