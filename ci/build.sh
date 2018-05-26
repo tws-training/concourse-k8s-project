43d630755ac2d056fa5ce6dd59c2de8917bf88a2
Seaborn Lee <seabornlee.cn@gmail.com>
2018-05-26 21:40:22 +0800

    Run jacocoTestReport

--- ci/build.sh
+++ ci/build.sh
@@ -5,6 +5,7 @@ cd git-resource
 # update version
 sed "s/DOCKER_VERSION/$(cat ../version/number)/g" ci/deployment.yml > ../build/deployment.yml
 
+gradle jacocoTestReport
 gradle sonarqube
 
 # package
