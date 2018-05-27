bddd4db3fbc33569aa15f60281df961805cd5d37
Seaborn Lee <seabornlee.cn@gmail.com>
2018-05-27 09:49:26 +0800

    FizzBuzz done

--- src/test/java/com/example/demo/dji/FizzBuzzTest.java
+++ src/test/java/com/example/demo/dji/FizzBuzzTest.java
@@ -11,4 +11,45 @@ public class FizzBuzzTest {
         String result = FizzBuzz.fizzBuzz(number);
         assertThat(result).isEqualTo("2");
     }
+
+    @Test
+    public void should_return_Fizz_given_number_divisible_by_3() throws Exception {
+        int number = 3;
+        String result = FizzBuzz.fizzBuzz(number);
+        assertThat(result).isEqualTo("Fizz");
+    }
+
+    @Test
+    public void should_return_Buzz_given_number_divisible_by_5() throws Exception {
+        int number = 5;
+        String result = FizzBuzz.fizzBuzz(number);
+        assertThat(result).isEqualTo("Buzz");
+    }
+
+    @Test
+    public void should_return_FizzBuzz_given_number_divisible_by_3_and_5() throws Exception {
+        int number = 15;
+        String result = FizzBuzz.fizzBuzz(number);
+        assertThat(result).isEqualTo("FizzBuzz");
+    }
+
+    @Test
+    public void should_return_FizzBuzz_given_number_0() throws Exception {
+        int number = 0;
+        String result = FizzBuzz.fizzBuzz(number);
+        assertThat(result).isEqualTo("FizzBuzz");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_exception_given_negative_number() throws Exception {
+        int number = -1;
+        String result = FizzBuzz.fizzBuzz(number);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_exception_given_number_greater_than_100() throws Exception {
+        int number = 101;
+        String result = FizzBuzz.fizzBuzz(number);
+    }
 }
